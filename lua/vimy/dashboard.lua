e73c226ab85e3148c23efb36867a18815883251f
Amine Neggazi <neggazimedlamine@gmail.com>
2023-01-03 12:29:15 +0100

    gd

--- lua/vimy/dashboard.lua
+++ lua/vimy/dashboard.lua
@@ -1,13 +1,24 @@
 local home = os.getenv('HOME')
 local db = require('dashboard')
 
--- linux
--- db.preview_command = 'cat | lolcat -F 0.3'
--- db.preview_file_height = 12
--- db.preview_file_width = 80
-
--- Dashboard settings
--- db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
+--
+-- Dashboard config
+--
+db.hide_statusline = true
+db.hide_tabline = true
+db.hide_winbar = true
+db.custom_header = {
+    '          ▀████▀▄▄              ▄█ ',
+    '            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ',
+    '    ▄        █          ▀▀▀▀▄  ▄▀  ',
+    '   ▄▀ ▀▄      ▀▄              ▀▄▀  ',
+    '  ▄▀    █     █▀   ▄█▀▄      ▄█    ',
+    '  ▀▄     ▀▄  █     ▀██▀     ██▄█   ',
+    '   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ',
+    '    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ',
+    '   █   █  █      ▄▄           ▄▀   ',
+    '                                   ',
+}
 db.custom_center = {
   {icon = '  ',
   desc = 'Recently opened files                   ',
@@ -26,3 +37,4 @@ db.custom_center = {
   action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
   shortcut = 'SPC f d'},
 }
+db.custom_footer = {}
