a14f26775a3fdc11c8b98d142f782f1b4f2e781b
Amine Neggazi <neggazimedlamine@gmail.com>
2023-01-03 11:19:43 +0100

    added packer

---
+++ lua/vimy/packer.lua
@@ -0,0 +1,74 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+  -- Dashboard
+  use 'glepnir/dashboard-nvim'
+
+	-- LSP setup
+	-- use {
+	-- 	'VonHeikemen/lsp-zero.nvim',
+	-- 	requires = {
+	-- 		-- LSP Support
+	-- 		{'neovim/nvim-lspconfig'},
+	-- 		{'williamboman/mason.nvim'},
+	-- 		{'williamboman/mason-lspconfig.nvim'},
+	--
+	-- 		-- Autocompletion
+	-- 		{'hrsh7th/nvim-cmp'},
+	-- 		{'hrsh7th/cmp-buffer'},
+	-- 		{'hrsh7th/cmp-path'},
+	-- 		{'saadparwaiz1/cmp_luasnip'},
+	-- 		{'hrsh7th/cmp-nvim-lsp'},
+	-- 		{'hrsh7th/cmp-nvim-lua'},
+	--
+	-- 		-- Snippets
+	-- 		{'L3MON4D3/LuaSnip'},
+	-- 		{'rafamadriz/friendly-snippets'},
+	-- 	}
+	-- }
+
+	-- Telescope
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	-- Treesitter
+	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+	-- Colorscheme
+	use 'folke/tokyonight.nvim'
+
+  -- Lualine
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  -- Tabline
+
+	-- Undotree
+	use 'mbbill/undotree'
+
+  -- Autopairs
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
+
+  -- Comment
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
+end)
