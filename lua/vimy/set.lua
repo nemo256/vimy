3c79b01fc4275e7d60de3f168cc264e320dbf3bf
Amine Neggazi <neggazimedlamine@gmail.com>
2023-01-05 00:50:35 +0100

    removed color_column option from nvim sets!

--- lua/vimy/set.lua
+++ lua/vimy/set.lua
@@ -14,7 +14,7 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = true
+vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -26,10 +26,8 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+-- vim.opt.updatetime = 50
+
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "no"
 vim.opt.isfname:append("@-@")
-
--- vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
