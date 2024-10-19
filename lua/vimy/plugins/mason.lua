94a36045554956e1d4edebb36f57043e874aa90c
Amine Neggazi <neggazimedlamine@gmail.com>
2024-10-19 21:44:19 +0100

    update mason

--- lua/vimy/plugins/mason.lua
+++ lua/vimy/plugins/mason.lua
@@ -1,33 +1,31 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    require("mason").setup()
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup()
 
-    require("mason-lspconfig").setup({
-      automatic_installation = true,
-      ensure_installed = {
-        "cssls",
-        "eslint",
-        "html",
-        "jsonls",
-        "pyright",
-        "tailwindcss",
-      },
-    })
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"cssls",
+				"eslint",
+				"html",
+				"jsonls",
+				"tailwindcss",
+			},
+		})
 
-    require("mason-tool-installer").setup({
-      ensure_installed = {
-        "prettier",
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    })
-  end,
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"prettier",
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"eslint_d",
+			},
+		})
+	end,
 }
