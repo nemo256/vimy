8b6a1daacfbf4e1a1b62cc002a80a9faedb3d24e
Amine Neggazi <neggazimedlamine@gmail.com>
2025-05-18 11:01:09 +0100

    update dashboard.nvim styles!

--- lua/vimy/plugins/dashboard.lua
+++ lua/vimy/plugins/dashboard.lua
@@ -1,58 +1,77 @@
 return {
-	"glepnir/dashboard-nvim",
-	event = "VimEnter",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("dashboard").setup({
-			theme = "doom",
-			config = {
-				header = {
-					"                                   ",
-					"          ▀████▀▄▄              ▄█ ",
-					"            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ",
-					"    ▄        █          ▀▀▀▀▄  ▄▀  ",
-					"   ▄▀ ▀▄      ▀▄              ▀▄▀  ",
-					"  ▄▀    █     █▀   ▄█▀▄      ▄█    ",
-					"  ▀▄     ▀▄  █     ▀██▀     ██▄█   ",
-					"   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ",
-					"    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ",
-					"   █   █  █      ▄▄           ▄▀   ",
-					"                                   ",
-					"                                   ",
-				},
-				center = {
-					{
-						icon = "  ",
-						desc = "History                      ",
-						key = "CTRL + SHIFT + H",
-						action = "Telescope oldfiles",
-					},
-					{
-						icon = "  ",
-						desc = "Files                        ",
-						key = "CTRL + SHIFT + F",
-						action = "Telescope find_files find_command=rg,--files",
-					},
-					{
-						icon = "  ",
-						desc = "Load                         ",
-						key = "CTRL + SHIFT + L",
-						action = "SessionLoad",
-					},
-					{
-						icon = "  ",
-						desc = "Work                         ",
-						key = "CTRL + SHIFT + W",
-						action = "e ~/work",
-					},
-				},
-				footer = { "󱓟  Neovim FTW  󱓟" },
-			},
-			hide = {
-				statusline = true,
-				tabline = true,
-				winbar = true,
-			},
-		})
-	end,
+  "glepnir/dashboard-nvim",
+  event = "UIEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    vim.cmd([[
+      highlight DashboardIcon guifg=#FFD700 guibg=NONE
+      highlight DashboardDesc guifg=#AAAAAA guibg=NONE
+      highlight DashboardKey guifg=#5FD7FF guibg=NONE
+    ]])
+
+    require("dashboard").setup({
+      theme = "doom",
+      config = {
+        header = {
+          "                                   ",
+          "          ▀████▀▄▄              ▄█ ",
+          "            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ",
+          "    ▄        █          ▀▀▀▀▄  ▄▀  ",
+          "   ▄▀ ▀▄      ▀▄              ▀▄▀  ",
+          "  ▄▀    █     █▀   ▄█▀▄      ▄█    ",
+          "  ▀▄     ▀▄  █     ▀██▀     ██▄█   ",
+          "   ▀▄    �▀ █   ▄██▄   ▄  ▄  ▀▀ █  ",
+          "    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ",
+          "   █   █  █      ▄▄           ▄▀   ",
+          "                                   ",
+          "                                   ",
+        },
+        center = {
+          {
+            icon = "  ",
+            icon_hl = "DashboardIcon",
+            desc = "History                      ",
+            desc_hl = "DashboardDesc",
+            key = "CTRL + SHIFT + H",
+            key_hl = "DashboardKey",
+            action = "Telescope oldfiles",
+          },
+          {
+            icon = "  ",
+            icon_hl = "DashboardIcon",
+            desc = "Files                        ",
+            desc_hl = "DashboardDesc",
+            key = "CTRL + SHIFT + F",
+            key_hl = "DashboardKey",
+            action = "Telescope find_files find_command=rg,--files",
+          },
+          {
+            icon = "  ",
+            icon_hl = "DashboardIcon",
+            desc = "Load                         ",
+            desc_hl = "DashboardDesc",
+            key = "CTRL + SHIFT + L",
+            key_hl = "DashboardKey",
+            action = "SessionLoad",
+          },
+          {
+            icon = "  ",
+            icon_hl = "DashboardIcon",
+            desc = "Work                         ",
+            desc_hl = "DashboardDesc",
+            key = "CTRL + SHIFT + W",
+            key_hl = "DashboardKey",
+            action = "e ~/work",
+          },
+        },
+        footer = { "  Neovim FTW  " },
+      },
+      hide = {
+        statusline = true,
+        tabline = true,
+        winbar = true,
+      },
+    })
+  end,
 }
+
